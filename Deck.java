223e3e06bd7f51308f3eecf8d8611b0a4fbb6408
lindhage22 <132088006+lindhage22@users.noreply.github.com>
2023-06-19 22:06:37 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -7,13 +7,13 @@ public class Deck {
     private List<Card> cards = new ArrayList<>();
     public Deck() {
 
-        List<String> suits = List.of("Clubs","Spades","Diamonds","Hearts");
+        List<String> suits = List.of("Hearts","Spades","Diamonds","Clubs");
         List<String> ranks = List.of("2","3","4","5","6","7","8","9","10","11 Jack",
                 "12 Queen","13 King","14 Ace");
         for(int i = 0; i< suits.size();i++){
-            for(int j = 0; j< ranks.size();j++){
-                String cardInfo = ranks.get(j) + " of" + suits.get(i);
-                  Card card = new Card(j,cardInfo);
+            for(int z = 0; z< ranks.size();z++){
+                String cardInfo = ranks.get(z) + " of" + suits.get(i);
+                  Card card = new Card(z,cardInfo);
                  cards.add(card);
                  
             }
